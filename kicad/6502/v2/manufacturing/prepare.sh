baee10c8b3699a437e28e5ade2fcb438de6a7d0d
David Banks <dave@hoglet.com>
2019-09-13 17:10:34 +0100

    Kicad v2: added build for manufacturing files
    Change-Id: I4a3f04d7ee044a9d6b395d2303a9b2331e0d8fa6

---
+++ kicad/6502/v2/manufacturing/prepare.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SRC=6502_adapter
+DST=6502_adapter
+
+mv $SRC-B_Cu.gbl      $DST.gbl
+mv $SRC-B_Mask.gbs    $DST.gbs
+mv $SRC-B_SilkS.gbo   $DST.gbo
+mv $SRC.drl           $DST.xln
+mv $SRC-Edge_Cuts.gm1 $DST.gko
+mv $SRC-F_Cu.gtl      $DST.gtl
+mv $SRC-F_Mask.gts    $DST.gts
+mv $SRC-F_SilkS.gto   $DST.gto
+
+rm -f manufacturing.zip
+zip -qr manufacturing.zip $DST.*
