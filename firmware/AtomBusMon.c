2773dd97b13667b0cee386b67668904d30207d60
David Banks <dave@hoglet.com>
2021-04-10 16:15:39 +0100

    Update firmware version to 0.993
    Change-Id: I5e261ecca2bf7c15a6fc4b05f5b90e5b5625a295

--- firmware/AtomBusMon.c
+++ firmware/AtomBusMon.c
@@ -14,7 +14,7 @@
  * VERSION and NAME are used in the start-up message
  ********************************************************/
 
-#define VERSION "0.992"
+#define VERSION "0.993"
 
 #if defined(CPU_Z80)
   #define NAME "ICE-Z80"
